626cb02bc8ba96d0a9e027535514c6ea95124c39
HumbleTheo <romariodonald1000@gmail.com>
2023-02-20 16:06:22 -0500

    Image in Sockets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,13 @@ import { Socket } from 'ngx-socket-io';
 })
 export class AppComponent {
   constructor(private socket: Socket) {
+    socket.on('image', (imageBuffer: string) => {
+      const image = document.getElementById('ig') as HTMLImageElement;
+      image.src = `data:image/*;base64,${imageBuffer}`;
+
+      console.log('A user is connected');
+    });
+
     socket.on('message', (sms: string) => {
       this.title = sms;
     });
