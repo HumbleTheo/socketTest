a3f51450ab97c7eb45abeb90b65bc3862a244d1c
HumbleTheo <romariodonald1000@gmail.com>
2023-02-21 09:34:40 -0500

    compress test

--- Websocket-Test/socket.js
+++ Websocket-Test/socket.js
@@ -7,12 +7,12 @@ const path = require("path");
 // Tells the socket server to listen on a specifc port
 socketServer.listen(process.env.PORT);
 
-// On Connection from a user emits a message in this case (Hello Wah Gwan Jamaica)
+// On Connection from a user emits a message
 socketServer.on("connection", function (socket) {
-	socket.emit("message", "Hello Wah Gwan Jamaica");
+  socket.compress(true).emit("compressed message", "Fast Car Display");
 
-	// Send an image file to client
-	const imagePath = path.join(__dirname, "/assets/Car.jpg");
-	const imageBuffer = fs.readFileSync(imagePath);
-	socket.emit("image", imageBuffer.toString("base64"));
+  // Send an image file to client
+  const imagePath = path.join(__dirname, "/assets/Car.jpg");
+  const imageBuffer = fs.readFileSync(imagePath);
+  socket.emit("image", imageBuffer.toString("base64"));
 });
